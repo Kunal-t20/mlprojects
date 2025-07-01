643ae649bdc2f24d27a9cfe613147e417a139219
KJM <kunalmahajan0202@gmail.com>
2025-07-02 01:09:34 +0530

    Hyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 import dill # to pick pkl file
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -20,18 +21,27 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evalute_models(X_train,y_train,X_test,y_test,models):
+def evalute_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
-        for i, (name, model) in enumerate(models.items()):
-            model.fit(X_train, y_train)   # Train
-            y_pred = model.predict(X_test)  # Predict
-            test_model_score = r2_score(y_test, y_pred)  # Evaluate
+        for name, model in models.items():
+            model_param_grid = param.get(name, {})  # get hyperparams for this model
 
-            report[name] = test_model_score  # ✅ Corrected line
+            if model_param_grid:
+                gs = GridSearchCV(model, model_param_grid, cv=3, n_jobs=-1, verbose=1, refit=True)
+                gs.fit(X_train, y_train)
 
-        
+                best_model = gs.best_estimator_
+            else:
+                model.fit(X_train, y_train)
+                best_model = model
+
+            y_pred = best_model.predict(X_test)
+            test_model_score = r2_score(y_test, y_pred)
+            report[name] = test_model_score
+
         return report
+    
     except Exception as e:
         raise CustomException(e,sys)
